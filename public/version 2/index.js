60ec431100e3727b4c869eadd221ca2280ff8064
Mystic-Wafflez <mysticmaya1337@gmail.com>
2024-04-08 18:52:21 +0200

    working on rotation

--- public/version 2/index.js
+++ public/version 2/index.js
@@ -1,3 +1,6 @@
+
+// TODO: fix player image rotation
+// made using HTML Canvas API
 // creates canvas
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d') 
@@ -12,8 +15,18 @@ class Player {
 	constructor({position, velocity}) {
 		this.position = position; // uses {x,y}
 		this.velocity = velocity; // same deal
+		this.rotation = 0
 	}
 	draw(){
+		// 'save' basically creates a savestate
+		// 'rotate' will try to rotate the entire canvas, so to rotate the player you need to change canvas' location to be on the player's center.
+		// this can be done by using 'translate', while referencing the player's x & y positions on the canvas.
+		// using 'save' & 'restore', the rotation code will only be called for whatever's within those two.
+		context.save()
+		context.translate(this.position.x, this.position.y)
+		context.rotate(this.rotation)
+		// reverts canvas back to original position
+		context.translate(-this.position.x, - this.position.y)
 		// player image variables
 		const playerHeight = 100;
 		const playerWidth = 100;
@@ -31,12 +44,14 @@ class Player {
 				playerHeight,
 				playerWidth)
 		}
+		context.restore()
 	}
 	// calls draw, updates position based on velocity
 	update() {
 		this.draw()
 		this.position.x += this.velocity.x
 		this.position.y += this.velocity.y
+		
 	}
 }
 // player
@@ -65,12 +80,15 @@ function animation() {
 	console.log(animation);
 	player.update()
 	// determines speed when key is pressed
+	player.velocity.x = 0;
 	if (keys.w.pressed) player.velocity.x = 1.64
 	
+	if (keys.d.pressed) player.rotation += 0.01
 }
 player.draw()
-console.log(player);
-animation();
+console.log(player)
+console.log(player.rotation)
+animation()
 
 window.addEventListener('keydown', (event) => {
 	// switch = cleaner if/else, uses cases. better readability.
@@ -80,9 +98,28 @@ window.addEventListener('keydown', (event) => {
 			console.log('Z input');
 			break
 		case 'KeyA':
+			keys.a.pressed = true
 			console.log('Q input');
 			break
 		case 'KeyD':
+			keys.d.pressed = true
+			console.log('D input');
+			break
+	}
+})
+// keyup stops input after letting go
+window.addEventListener('keyup', (event) => {
+	switch(event.code) {
+		case 'KeyW':
+			keys.w.pressed = false
+			console.log('Z input');
+			break
+		case 'KeyA':
+			keys.a.pressed = false
+			console.log('Q input');
+			break
+		case 'KeyD':
+			keys.d.pressed = false
 			console.log('D input');
 			break
 	}
